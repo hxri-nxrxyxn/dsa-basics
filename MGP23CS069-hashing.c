88b1ef3744195822c56eff89fadfc76e4512bfaa
Hari Narayan <harinarayanmr@aol.com>
2024-11-04 15:29:49 +0530

    removed comments frome hashing

--- MGP23CS069-hashing.c
+++ MGP23CS069-hashing.c
@@ -56,25 +56,3 @@ void main()
 			printf("%d\tempty\n", i);
 	}
 }
-
-// read elements into arr
-// try to find memory locations by hashing function
-// k = arr[i]
-// loc = k % m
-// 
-// if collision occurs
-// if k % m != any loc
-//	loc = k % m
-// if k %m == any loc
-//	loc = k + 1 % m
-//
-//	do k + 1 % m
-
-// how to find m?
-// size = 0 m = np
-// size = 1...5 m = 5
-// size = 5...10 m = 10
-// size = 10...15 m = 15
-// size = 100.. 105 = 105
-// if size <= multiple of 5 and next multiple of 5, m= next multiple of 5
-// size + i % 5 == 0 
