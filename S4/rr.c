8c510ea10371ff6487b479b81ef569b50819b9f1
Hari Narayan <97329430+hxri-nxrxyxn@users.noreply.github.com>
2025-10-21 21:19:38 +0530

    feat: updated rr

--- S4/rr.c
+++ S4/rr.c
@@ -1,66 +1,88 @@
-#include <stdio.h>
-#include <limits.h>
-
-struct process {
-    int id, at, bt, ct, tat, wt, remaining_bt, stat;
+#include<stdio.h>
+struct process{
+    int id,at,bt,ct,tat,wt;
+    int status,qstatus,r_time;
 };
-
-int main() {
-    struct process p[20];
-    int n, i, quantum, comp = 0, current_time = 0;
-    float tottat = 0, totwt = 0, avgtat = 0, avgwt = 0;
-
-    printf("Enter Number of Processes : ");
-    scanf("%d", &n);
-    printf("Enter Time Quantum: ");
-    scanf("%d", &quantum);
-
-    printf("Enter Process Details : \n");
-    for (i = 0; i < n; i++) {
-        printf("Enter id | at | bt :\t");
-        scanf("%d%d%d", &p[i].id, &p[i].at, &p[i].bt);
-        p[i].remaining_bt = p[i].bt;
-        p[i].stat = 0; // Process is not completed initially
+//Queue
+int q[100],front=-1,rear=-1;
+void enqueue(int item){
+    if(rear == -1){
+        rear++;
+        front++;
+        q[rear]=item;
     }
+    else{
+        rear++;
+        q[rear]=item;
+    }
+}
+int dequeue(){
+    int item;
+    item=q[front];
+    front++;
+    return item;
+}
 
-    while (comp != n) {
-        int all_completed = 1;
-       
-        for (i = 0; i < n; i++) {
-            if (p[i].at <= current_time && p[i].stat == 0) {
-                all_completed = 0; // At least one process is not completed
-                if (p[i].remaining_bt > quantum) {
-                    current_time += quantum;
-                    p[i].remaining_bt -= quantum;
-                } else {
-                    current_time += p[i].remaining_bt;
-                    p[i].ct = current_time;
-                    p[i].tat = p[i].ct - p[i].at;
-                    p[i].wt = p[i].tat - p[i].bt;
-                    tottat += p[i].tat;
-                    totwt += p[i].wt;
-                    p[i].stat = 1; // Mark process as completed
-                    comp++;
-                    p[i].remaining_bt = 0;
-                }
+int main(){
+    struct process p[30];
+    int i,j,n,completed = 0,tq,current_time;
+    printf("Enter the no of process:");
+    scanf("%d",&n);
+    printf("Enter the time quantum: ");
+    scanf("%d",&tq);
+
+    for(i=0;i<n;i++){
+        printf("enter id, at, bt\n");
+        scanf("%d %d %d",&p[i].id,&p[i].at,&p[i].bt);
+        p[i].status=0;
+        p[i].qstatus=0;
+        p[i].r_time=p[i].bt;
+    }
+    enqueue(0);
+    p[0].qstatus=1;
+    current_time = p[0].at;
+    while(completed != n){
+        int i = dequeue();
+        if(p[i].r_time > tq){
+            current_time += tq;
+            p[i].r_time -= tq;
+        }
+        else{
+            current_time += p[i].r_time;
+            p[i].ct = current_time;
+            p[i].status = 1;
+            completed ++;
+            p[i].tat = p[i].ct - p[i].at;
+            p[i].wt = p[i].tat - p[i].bt-(p[i].bt-p[i].r_time);
+            p[i].r_time=0;
+        }
+        for(int j=i+1;j<n;j++){
+            if(p[j].at <= current_time && p[j].status==0 && p[j].qstatus==0){
+                enqueue(j);
+                p[j].qstatus=1;
             }
         }
-
-        if (all_completed) {
-            current_time++;
+        if(p[i].r_time != 0){
+            enqueue(i);
         }
+
     }
-
-    avgtat = tottat / n;
-    avgwt = totwt / n;
-
-    printf("ID\tAT\tBT\tCT\tTAT\tWT\n");
-    for (i = 0; i < n; i++) {
-        printf("%d \t%d \t%d \t%d \t%d \t%d\n", p[i].id, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    float tot_tat,tot_wt;
+    float avg_tat = 0.0;
+    float avg_wt = 0.0;
+    for(i=0;i<n;i++){
+        tot_tat += p[i].tat;
+        tot_wt += p[i].wt;
     }
+    avg_tat = tot_tat/n;
+    avg_wt = tot_wt/n;
 
-    printf("Average Turn Around Time : %f\n", avgtat);
-    printf("Average Waiting Time     : %f\n", avgwt);
-
-    return 0;
+    // process details
+    printf("Process details\n");
+    printf("id\tat\tbt\tct\ttat\twt\n");
+    for(i=0;i<n;i++){
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
+    }
+    printf("Average tat = %0.2f\n",avg_tat);
+    printf("Average wt = %0.2f\n",avg_wt);
 }
